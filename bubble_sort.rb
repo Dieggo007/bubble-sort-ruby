d315a8afcaaf716ad8afe7506486b66dab9c0dca
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-03-06 13:24:38 -0500

    add everything

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+
+def bubble_sort(array)
+  i = 0
+  limit = array.length - 1
+  while i < limit
+    if array[i] > array[i + 1]
+      temp = array[i + 1]
+      array[i + 1] = array[i]
+      array[i] = temp
+    end
+    if i == limit - 1
+      i = 0
+      limit -= 1
+      next
+    end
+    i += 1
+  end
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2,30,7,-4,200,8,10,13])
